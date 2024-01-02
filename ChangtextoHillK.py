a174892f56cee042b0397d209c6f6912d4114ddd
AndyAnh174 <hovietanh147@gmail.com>
2024-01-03 02:37:28 +0700

    add change text

---
+++ ChangtextoHillK.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def encrypt_hill_cipher(plaintext, key_matrix):
+    text_numbers = [ord(char) - ord('A') for char in plaintext]
+    n = len(key_matrix)
+
+    while len(text_numbers) % n != 0:
+        text_numbers.append(ord('X') - ord('A'))
+
+    text_blocks = [text_numbers[i:i + n] for i in range(0, len(text_numbers), n)]
+
+    encrypted_text = ""
+    for block in text_blocks:
+        encrypted_block = np.dot(key_matrix, block) % 26
+        encrypted_text += ''.join([chr(char + ord('A')) for char in encrypted_block])
+
+    return encrypted_text
+
+plaintext = "0949000030MBBANK"
+key_matrix = np.array([[6, 2], [3, 1]])
+
+encrypted_text = encrypt_hill_cipher(plaintext, key_matrix)
+print("Văn bản mã hóa:", encrypted_text)
